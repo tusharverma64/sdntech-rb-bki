745284d37b145b0d8f01664abe565473d4be6964
Mohit Soni <mohit.soni@liferay.com>
2021-04-15 16:35:17 +0530

    Public Commit

---
+++ rb-workspace/modules/bouki-hello-world/src/main/java/com/rb/bouki/hello/world/constants/BoukiHelloWorldPortletKeys.java
@@ -0,0 +1,11 @@
+package com.rb.bouki.hello.world.constants;
+
+/**
+ * @author Mohit
+ */
+public class BoukiHelloWorldPortletKeys {
+
+	public static final String BOUKIHELLOWORLD =
+		"com_rb_bouki_hello_world_BoukiHelloWorldPortlet";
+
+}
---
+++ rb-workspace/modules/bouki-hello-world/src/main/java/com/rb/bouki/hello/world/portlet/BoukiHelloWorldPortlet.java
@@ -0,0 +1,30 @@
+package com.rb.bouki.hello.world.portlet;
+
+import com.rb.bouki.hello.world.constants.BoukiHelloWorldPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Mohit
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=BoukiHelloWorld",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + BoukiHelloWorldPortletKeys.BOUKIHELLOWORLD,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class BoukiHelloWorldPortlet extends MVCPortlet {
+}
